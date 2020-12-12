004666fbb7250c6c0533802dd1ed3d72443f8e09
Nischal Paudyal <49691309+npaudyal@users.noreply.github.com>
2020-12-12 13:40:14 -0600

    Update Balanced String Split.java

--- Balanced String Split.java
+++ Balanced String Split.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public int balancedStringSplit(String s) {
+        
+        int balancedCount =0;
+        int count =0;
+        for(int i=0; i<s.length(); i++) {
+            char a = s.charAt(i);
+            if(a == 'L') {
+                count++;
+            } else if(a == 'R') {
+                count--;
+            } if(count == 0) {
+                        balancedCount++;
+            } 
+        }
+        
+        return balancedCount;
+        
+    }
+}
