325a2bbcb1a68daf682b9e53451df2f118002c16
Nischal Paudyal <49691309+npaudyal@users.noreply.github.com>
2021-01-04 22:34:59 -0600

    Create sorted_stack.java

---
+++ sorted_stack.java
@@ -0,0 +1,27 @@
+class SortStack 
+{ 
+    // This function return the sorted stack 
+    public static Stack<Integer> sortstack(Stack<Integer>  
+                                             input) 
+    { 
+        Stack<Integer> tmpStack = new Stack<Integer>(); 
+        while(!input.isEmpty()) 
+        { 
+            // pop out the first element 
+            int tmp = input.pop(); 
+          
+            // while temporary stack is not empty and 
+            // top of stack is greater than temp 
+            while(!tmpStack.isEmpty() && tmpStack.peek()  
+                                                 > tmp) 
+            { 
+                // pop from temporary stack and  
+                // push it to the input stack 
+            input.push(tmpStack.pop()); 
+            } 
+              
+            // push temp in tempory of stack 
+            tmpStack.push(tmp); 
+        } 
+        return tmpStack; 
+    } 
